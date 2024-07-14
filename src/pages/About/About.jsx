223f94fb301a15b3e68ee5272b5ed7a62c90ef1c
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-07-14 08:04:48 +0700

    upload first

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 import "./About.css";
-import aboutImg from "../../images/about-img.jpg";
+import aboutImg from "../../images/about.jpg";
 
 const About = () => {
   return (
-    <section className='about'>
-      <div className='container'>
-        <div className='section-title'>
+    <section className="about">
+      <div className="container">
+        <div className="section-title">
           <h2>About</h2>
         </div>
 
-        <div className='about-content grid'>
-          <div className='about-img'>
-            <img src = {aboutImg} alt = "" />
+        <div className="about-content grid">
+          <div className="about-img">
+            <img src={aboutImg} alt="About ThievBook" />
           </div>
-          <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dignissimos consequuntur vero commodi provident maiores, iusto atque corrupti voluptate vel sequi consectetur unde aliquam corporis saepe animi non, tempora reiciendis molestias sed laudantium dolores. Assumenda aperiam fuga quo voluptate commodi ullam aliquam expedita voluptas delectus.</p>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+          <div className="about-text">
+            <h2 className="about-title fs-26 ls-1">About ThievBook</h2>
+            <p className="fs-17">
+              Welcome to ThievBook, your ultimate online book library. Our platform provides access to
+              a vast collection of books across various genres and topics. Whether you're looking for
+              the latest bestsellers, classic literature, or educational materials, ThievBook has
+              something for everyone.
+            </p>
+            <p className="fs-17">
+              Our mission is to make reading more accessible and enjoyable for everyone. We believe the
+              power of books to educate, inspire and entertain. Start exploring the endless
+              possibilities of the written word.
+            </p>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
